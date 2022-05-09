a2924f1c6aa8dcccf6e59cac51b7afa7332321ac
Jenny Aguilar <contact@jaguilarweb.com>
2022-05-09 16:30:45 +0200

    refactor: Change navigation way adding NavBar component and the routerLink attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostItemComponent } from './post-item/post-item.component';
 import { HistoryComponent } from './history/history.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
     PostItemComponent,
-    HistoryComponent
+    HistoryComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
