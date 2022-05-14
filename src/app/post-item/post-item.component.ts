16522eb7d8d5dec123cb54989841db4a017d09d3
Jenny Aguilar <contact@jaguilarweb.com>
2022-05-14 10:57:57 +0200

    style: Change style plus add Bootstrap dependency

--- src/app/post-item/post-item.component.ts
+++ src/app/post-item/post-item.component.ts
@@ -23,12 +23,14 @@ export class PostItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  upvote(post:Post): void {
+  upvote(post:Post): Post {
     post.votes += 1;
+    return post;
   }
 
-  downvote(post:Post): void {
+  downvote(post:Post): Post {
     post.votes -= 1;
+    return post;
   }
 
   hide(post: Post): void {
